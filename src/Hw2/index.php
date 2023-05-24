a6b3eb786bd07ee7d1ea13a8e8bc715881c53bfa
Sandro Ghughunishvili <84451463+godfather-coder@users.noreply.github.com>
2023-05-24 07:34:11 -0700

    2

---
+++ src/Hw2/index.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+echo "TASK N1".'<br>';
+
+$data =[
+    "temo" => 23,
+    "nika" => 32,
+    "giorgi" => 45,
+    "lia" => 28,
+    "mariami" =>15,
+    "tako" => 35,
+    "vako" => 55
+];
+foreach ($data as $pearson => $age){
+    if($age >=30){
+        echo $pearson.'<br>';
+    }
+}
+echo "// // // // // // // // // // // // // // // // //".'<br>';;
+echo "TASK N2".'<br>';
+$intdata = [
+    "12",
+    "32",
+    "118",
+    "15",
+    "3",
+];
+function multByTwo(int $element) :int{
+    return $element * 2;
+}
+foreach($intdata as $item){
+    echo multByTwo((int)$item).'<br>';
+}
